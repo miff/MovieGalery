69c9c3f67c7b1a6f1d005213b573b0ec7f053c96
Milan Djordjevic <milan.djordjevic@vegait.rs>
2024-03-13 09:06:43 +0100

    Initial Commit

---
+++ MoviewGallery/MoviewGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviewGalleryApp.swift
+//  MoviewGallery
+//
+//  Created by Milan Djordjevic on 13/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoviewGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
